9ed10074eba57538384fa30e0712948a401dd96f
Gurnoor Singh Khurana <gurnoor6@gmail.com>
2020-05-28 13:15:41 +0530

    added angular part

--- backend/eduapp/settings.py
+++ backend/eduapp/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'rest_framework'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
